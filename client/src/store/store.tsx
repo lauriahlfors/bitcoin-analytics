b137690c7968efb0d6eede0e55575b3fb3efd38b
lauriahlfors <ahlforslauri@gmail.com>
2022-01-11 00:15:24 +0200

    update: store.tsx with new helper functions and imports

--- client/src/store/store.tsx
+++ client/src/store/store.tsx
@@ -1,6 +1,23 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { ICoinGecko } from './types';
 
-const useStore = () => {};
+import { getCoinGeckoData } from './bitcoin.functions';
+
+const useStore = () => {
+  // coin gecko data state
+  const [coinGeckoData, setCoinGeckoData] = useState<ICoinGecko>(
+    {} as ICoinGecko
+  );
+  // coin gecko url state
+  const [coinGeckoUrl, setCoinGeckoUrl] = useState<string>(
+    'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1641075131&to=1641852731'
+  );
+
+  return {
+    setApiUrl: () => {},
+    getApiData: () => getCoinGeckoData(coinGeckoUrl).then(setCoinGeckoData),
+  };
+};
 
 // initialize store context
 const StoreContext = createContext<ReturnType<typeof useStore> | null>(null);
